3d27adc276003184ad23185bd1484c161f564b9b
johnjarellano <john.j.arellano@gmail.com>
2015-09-29 19:39:44 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
   
+  has_many :microposts, dependent: :destroy
+  
   # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -64,6 +66,12 @@ class User < ActiveRecord::Base
   
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  
+  # Defines a proto-feed.
+  # See "Following users" for the full implementation.
+  def feed
+    Micropost.where("user_id = ?", id)
   end
   
   private
